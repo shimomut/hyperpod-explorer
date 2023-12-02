4e751da93c29b917638251a3cfee5ade5783d0f6
Tomonori Shimomura <shimomut@amazon.com>
2023-12-01 20:51:24 -0800

    markdown for cluster and instance group

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import time
+import json
 
 from rich.syntax import Syntax
 from rich.traceback import Traceback
@@ -138,6 +138,8 @@ class HyperPodExplorer(App):
 
             # Left tree pane            
             tree: Tree[dict] = Tree("My clusters", id="tree-view")
+            
+            tree.root.boto3_data = {}
             tree.root.expand()
 
             clusters = self.hyperpod_client.list_clusters()
@@ -172,35 +174,25 @@ class HyperPodExplorer(App):
 
         event.stop()
 
+        details_view = self.query_one("#details", Markdown)
+
         boto3_data = event.node.boto3_data
 
         if "Cluster" in boto3_data:
-
-            details_view = self.query_one("#details", Markdown)
-
-            details_view.update(markdown=EXAMPLE_MARKDOWN1)
-
-            self.query_one("#right-pane").scroll_home(animate=False)
-            self.sub_title = str(id(event))
+            markdown = self.format_cluster_markdown( boto3_data["Cluster"] )
+            details_view.update(markdown=markdown)
 
         elif "InstanceGroup" in boto3_data:
-
-            details_view = self.query_one("#details", Markdown)
-
-            details_view.update(markdown=EXAMPLE_MARKDOWN2)
-
-            self.query_one("#right-pane").scroll_home(animate=False)
-            self.sub_title = str(id(event))
-
+            markdown = self.format_instance_group_markdown( boto3_data["InstanceGroup"] )
+            details_view.update(markdown=markdown)
 
         elif "ClusterNode" in boto3_data:
-
-            details_view = self.query_one("#details", Markdown)
-
             details_view.update(markdown=EXAMPLE_MARKDOWN3)
+        
+        else:
+            return
 
-            self.query_one("#right-pane").scroll_home(animate=False)
-            self.sub_title = str(id(event))
+        self.query_one("#right-pane").scroll_home(animate=False)
 
     def on_markdown_link_clicked(self, event: Markdown.LinkClicked) -> None:
 
@@ -210,6 +202,75 @@ class HyperPodExplorer(App):
 
     def action_toggle_tree_pane(self) -> None:
         self.show_tree = not self.show_tree
+
+    def format_cluster_markdown(self, cluster):
+
+        cluster_name = cluster["ClusterName"]
+        cluster_status = cluster['ClusterStatus']
+        creation_time = cluster["CreationTime"].strftime("%Y/%m/%d %H:%M:%S")
+        arn = cluster["ClusterArn"]
+
+        lines = []
+        
+        lines.append( f"## Cluster - [{cluster_name}]" )
+
+        lines.append( f"#### Status" )
+
+        lines.append( f"| Name | Value |" )
+        lines.append( f"| ---- | ----- |" )
+        lines.append( f"| Name | {cluster_name} |" )
+        lines.append( f"| Status | {cluster_status} |" )
+        lines.append( f"| Creation time | {creation_time} |" )
+        lines.append( f"| Arn | {arn} |" )
+
+        if cluster_status=="Failed":
+
+            message = json.loads(cluster["FailureMessage"])
+
+            lines.append( f"#### Failure message" )
+            lines.append( f"```" )
+            for line in message["errorMessage"].splitlines():
+                print(f"{line}")
+            lines.append( f"```" )
+
+        return "\n".join(lines)
+
+
+    def format_instance_group_markdown(self, instance_group):
+
+        instance_group_name = instance_group["InstanceGroupName"]
+        current_count = instance_group["CurrentCount"]
+        target_count = instance_group["CurrentCount"]
+        instance_type = instance_group["InstanceType"]
+        threads_per_core = instance_group["ThreadsPerCore"]
+        role_arn = instance_group["ExecutionRole"]
+        lcc_s3_uri = instance_group["LifeCycleConfig"]["SourceS3Uri"]
+        creatation_script = instance_group["LifeCycleConfig"]["OnCreate"]
+
+        lines = []
+        
+        lines.append( f"## Instance group - [{instance_group_name}]" )
+
+        lines.append( f"#### Status" )
+
+        lines.append( f"| Name | Value |" )
+        lines.append( f"| ---- | ----- |" )
+        lines.append( f"| Name | {instance_group_name} |" )
+        lines.append( f"| Count | {current_count} / {target_count} |" )
+        lines.append( f"| Type | {instance_type} |" )
+        lines.append( f"| Threads per core | {threads_per_core} |" )
+        lines.append( f"| IAM Role | {role_arn} |" )
+
+        lines.append( f"" )
+
+        lines.append( f"#### Creation configuration" )
+
+        lines.append( f"| Name | Value |" )
+        lines.append( f"| ---- | ----- |" )
+        lines.append( f"| S3 Uri | {lcc_s3_uri} |" )
+        lines.append( f"| Creation script | {creatation_script} |" )
+
+        return "\n".join(lines)
 
 
 if __name__ == "__main__":
